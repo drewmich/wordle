440fa981de7027766bbd1b2f4cae78fb9bd7eb56
Drew Michelini <dbmich@umich.edu>
2022-02-09 20:52:04 -0500

    fixes to set_dif, still not filling result container

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ using namespace std;
 
 class Wordle{
 public:
-    set<string> dictionary;
-    set<string> working_set;
-    unordered_map<char, unordered_map<int, set<string>>> map;
+    vector<string> dictionary;
+    vector<string> working_set;
+    unordered_map<char, unordered_map<int, vector<string>>> map;
     
     int possible_words = -1;
     
@@ -70,8 +70,8 @@ public:
         while (getline(file, word)){
             
             //string* word_ptr = &word;
-            dictionary.insert(word);
-            cout << word << endl;
+            dictionary.push_back(word);
+            //cout << word << endl;
             
             // Populate map
             
@@ -82,15 +82,17 @@ public:
                 char letter = word[j];
                 
                 // whole set
-                map[letter][0].insert(word);
+                map[letter][0].emplace_back(word);
                 
                 // index set
-                map[letter][j + 1].insert(word);
+                map[letter][j + 1].emplace_back(word);
                 
             }
             
             
         }
+        
+        working_set.reserve(dictionary.size());
         
         cout << "Ready" << endl;
         
@@ -102,15 +104,26 @@ public:
         
         for (auto x : green){
             // Narrow dictionary down to only words with letter at correct index
+            sort(dictionary.begin(), dictionary.end());
+            sort(map[guess[x]][0].begin(), map[guess[x]][0].end());
+            
             set_intersection(dictionary.begin(), dictionary.end(), map[guess[x]][0].begin(), map[guess[x]][0].end(), working_set.begin());
+            
+            sort(working_set.begin(), working_set.end());
             dictionary = working_set;
             working_set = {};
             grays.erase(x);
         }
             
         for (auto x : yellow){
+            //size_t x = static_cast<size_t>(y);
             // Narrow dictionary down by elim words that have letter at spec index
+            sort(dictionary.begin(), dictionary.end());
+            sort(map[guess[x]][x + 1].begin(), map[guess[x]][x + 1].end());
+            
             set_difference(dictionary.begin(), dictionary.end(), map[guess[x]][x + 1].begin(), map[guess[x]][x + 1].end(), working_set.begin());
+            
+            sort(working_set.begin(), working_set.end());
             dictionary = working_set;
             working_set = {};
             grays.erase(x);
@@ -120,7 +133,12 @@ public:
         // Narrow down dictionary by eliminating gray letters
         for (auto x : grays){
             
+            sort(dictionary.begin(), dictionary.end());
+            sort(map[guess[x]][0].begin(), map[guess[x]][0].end());
+            
             set_difference(dictionary.begin(), dictionary.end(), map[guess[x]][0].begin(), map[guess[x]][0].end(), working_set.begin());
+            
+            sort(working_set.begin(), working_set.end());
             dictionary = working_set;
             working_set = {};
             
@@ -151,23 +169,22 @@ public:
                 
             }
             
-            cout << "Enter indices of green tiles, when done enter -1: ";
-            while (green != -1){
-                cin >> green;
+            cout << "Enter indices of green tiles, when done enter -1: " << endl;
+            while (cin >> green){
                 if (green == -1) { break; }
                 g.push_back(green);
             }
             
-            cout << "Enter indices of yellow tiles, when done enter -1: ";
-            while (yellow != -1){
-                cin >> yellow;
+            cout << "Enter indices of yellow tiles seperated by spaces: " << endl;
+            while (cin >> yellow){
                 if (yellow == -1) { break; }
-                y.push_back(green);
+                y.push_back(yellow);
             }
             
             fillSet(guess, y, g);
             
             // If set size < 10 print words
+            cout << "dict size: " << dictionary.size() << endl;
             if (dictionary.size() <= 10){
                 cout << "Possible words:" << endl;
                 
@@ -189,6 +206,14 @@ int main(int argc, char *argv[]) {
     
     ios_base::sync_with_stdio(false);
     //xcode_redirect(argc, argv);
+    
+//    vector<int> one = {1, 2, 3, 4, 5};
+//    vector<int> two = {3, 4};
+//    vector<int> result(10);
+//
+//    set_difference(one.begin(), one.end(), two.begin(), two.end(), result.begin());
+    
+    
     
     Wordle p;
     
