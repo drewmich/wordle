d7ddbd957d6870a918bda1ab7c416f2a42ed5d22
Drew Michelini <dbmich@umich.edu>
2022-02-14 12:08:01 -0500

    having issues

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ public:
     };
     
     struct Comp_s{
-        bool operator()(Tile* t1, Tile* t2){
+        bool operator()(Tile* t1, Tile* t2) const{
             
             return t1->word < t2->word;
             
@@ -48,13 +48,21 @@ public:
         }
     };
     
-    priority_queue<Tile*, vector<Tile*>, Comp_o> nextWord;
+    
     vector<Tile*> data;
     set<Tile*, Comp_s> dictionary;
     set<Tile*, Comp_s> working_set;
-    unordered_map<char, unordered_map<int, set<Tile*, Comp_s>>> map;
+    set<Tile*, Comp_s> possibleGuesses;
+    unordered_map<char, unordered_map<int, set<Tile*, Comp_s> > > map;
+    // set<char> remainingLetters = {
+    //     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+    //     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+    //     'u', 'v', 'w', 'x', 'y', 'z'
+    //     };
+
+    set<char> guessedLetters;
     
-    int possible_words = -1;
+    // int possible_words = -1;
     
     void get_options(int argc, char** argv) {
         int option_index = 0, option = 0;
@@ -104,8 +112,9 @@ public:
             Tile* tile_ptr = new Tile(word, i);
             
             //string* word_ptr = new string(word);
-            nextWord.push(tile_ptr);
+            //nextWord.push(tile_ptr);
             dictionary.insert(tile_ptr);
+            possibleGuesses.insert(tile_ptr);
  
             // Populate map
             for (int j = 0; j < 5; ++j){
@@ -130,57 +139,86 @@ public:
     
     void fillSet(string guess, vector<int> yellow, vector<int> green){
         
-        unordered_set<int> grays = {0, 1, 2, 3, 4};
+        vector<int> tg;
+        tg.push_back(1);
+        tg.push_back(2);
+        tg.push_back(3);
+        tg.push_back(4);
+        tg.push_back(5);
+        unordered_set<int> grays (tg.begin(), tg.end());
         
         for (auto x : green){
+
+            set<Tile*, Comp_s> tempSet;
+            guessedLetters.insert(x);
+            set_difference(possibleGuesses.begin(), possibleGuesses.end(), map[guess[x]][0].begin(), map[guess[x]][0].end(), inserter(tempSet, tempSet.begin()));
+            possibleGuesses = tempSet;
             
             // Narrow dictionary down to only words with letter at correct index
             set_intersection(dictionary.begin(), dictionary.end(), map[guess[x]][x + 1].begin(), map[guess[x]][x + 1].end(), inserter(working_set, working_set.begin()));
 
             dictionary = working_set;
-            working_set = {};
+            working_set.clear();
             grays.erase(x);
         }
             
         for (auto x : yellow){
 
+            set<Tile*, Comp_s> tempSet;
+            guessedLetters.insert(x);
+            set_difference(possibleGuesses.begin(), possibleGuesses.end(), map[guess[x]][0].begin(), map[guess[x]][0].end(), inserter(tempSet, tempSet.begin()));
+            possibleGuesses = tempSet;
+
             // Narrow dictionary down by elim words that have letter at spec index
             set_difference(dictionary.begin(), dictionary.end(), map[guess[x]][x + 1].begin(), map[guess[x]][x + 1].end(), inserter(working_set, working_set.begin()));
             
             dictionary = working_set;
-            working_set = {};
+            working_set.clear();
             
             // Narrow dict down to only words with letter at other indices
             set_intersection(dictionary.begin(), dictionary.end(), map[guess[x]][0].begin(), map[guess[x]][0].end(), inserter(working_set, working_set.begin()));
             
             dictionary = working_set;
-            working_set = {};
+            working_set.clear();
             grays.erase(x);
         }
             
         for (auto x : grays){
+
+            set<Tile*, Comp_s> tempSet;
+            guessedLetters.insert(x);
+            set_difference(possibleGuesses.begin(), possibleGuesses.end(), map[guess[x]][0].begin(), map[guess[x]][0].end(), inserter(tempSet, tempSet.begin()));
+            possibleGuesses = tempSet;
             
             // Narrow down dictionary by eliminating gray letters
             set_difference(dictionary.begin(), dictionary.end(), map[guess[x]][0].begin(), map[guess[x]][0].end(), inserter(working_set, working_set.begin()));
             
             dictionary = working_set;
-            working_set = {};
+            working_set.clear();
             
         }
         
     }
+
+    // string nextGuess(){
+
+    //     // words that do not contain any guessed letters
+    //     // guessedLetters set
+    //     // possibleGuesses set, set_difference for each letter guessed
+
+    // }
     
     void prompt(){
         
-        working_set = {};
+        working_set.clear();
         
         for (int i = 0; i < 6; ++i){
             
             string guess;
             int yellow = -2;
             int green = -2;
-            vector<int> g = {};
-            vector<int> y = {};
+            vector<int> g;
+            vector<int> y;
             
             cout << "---------" << endl;
             cout << "Enter guess #" << i + 1 << ": ";
@@ -208,7 +246,7 @@ public:
             fillSet(guess, y, g);
             
             // If set size < 10 print words
-            cout << "dict size: " << dictionary.size() << endl;
+            // cout << "dict size: " << dictionary.size() << endl;
             
             if (dictionary.size() == 1){
                 
@@ -219,6 +257,22 @@ public:
                 return;
                 
             }
+
+            cout << "Suggested next guess: ";
+
+            priority_queue<Tile*, vector<Tile*>, Comp_o> nextWord(possibleGuesses.begin(), possibleGuesses.end());
+
+            size_t yh = nextWord.size();
+            int tel = nextWord.size();
+            for (int i = 0; i < tel; ++i){
+                cout << nextWord.top()->word << endl;
+                nextWord.pop();
+            }
+            //cout << nextWord.top()->word << endl;
+            cout << yh << endl;
+            
+
+
             
             if (dictionary.size() <= 10){
                 cout << "Possible words:" << endl;
